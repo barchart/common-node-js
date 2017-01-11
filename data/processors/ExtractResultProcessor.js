840510d5c07258094f6a758a93ebb654fb588324
Bryan Ingle <bryan.ingle@barchart.com>
2017-01-11 13:59:10 -0600

    Document ExtractResultProcessor

--- data/processors/ExtractResultProcessor.js
+++ data/processors/ExtractResultProcessor.js
@@ -10,6 +10,14 @@ module.exports = (() => {
 
 	const logger = log4js.getLogger('data/processors/ExtractResultProcessor');
 
+	/**
+	 * Maps the results into a new array, selecting a single property from each item.
+	 *
+	 * @public
+	 * @extends ResultProcessor
+	 * @param {object} configuration
+	 * @param {string} configuration.propertyName - The name of the property to select from each item.
+	 */
 	class ExtractResultProcessor extends ResultProcessor {
 		constructor(configuration) {
 			super(configuration);
