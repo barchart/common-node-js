be27da552da61f3ef86685d3c00ac7059b3ccd74
Bryan Ingle <bryan.ingle@barchart.com>
2017-01-26 15:53:47 -0600

    Enhance S3 provider

--- aws/S3Provider.js
+++ aws/S3Provider.js
@@ -3,12 +3,32 @@ var log4js = require('log4js');
 
 var assert = require('common/lang/assert');
 var Disposable = require('common/lang/Disposable');
+var is = require('common/lang/is');
 
 module.exports = (() => {
 	'use strict';
 
 	const logger = log4js.getLogger('common-node/messaging/S3Provider');
 
+	const mimeTypes = {
+		text: 'text/plain',
+		html: 'text/html',
+		json: 'application/json'
+	};
+	
+	const encodingTypes = {
+		utf8: 'utf-8'
+	};
+
+	/**
+	 * Wrapper for Amazon's S3 SDK.
+	 *
+	 * @public
+	 * @extends Disposable
+	 * @param {object} configuration
+	 * @param {string} configuration.region
+	 * @param {string=} configuration.apiVersion
+	 */
 	class S3Provider extends Disposable {
 		constructor(configuration) {
 			super();
@@ -25,6 +45,12 @@ module.exports = (() => {
 			this._started = false;
 		}
 
+		/**
+		 * Connects to Amazon. Must be called once before using other instance
+		 * functions.
+		 *
+		 * @returns {Promise.<boolean>}
+		 */
 		start() {
 			if (this.getIsDisposed()) {
 				throw new Error('The S3 Provider has been disposed.');
@@ -35,7 +61,7 @@ module.exports = (() => {
 					.then(() => {
 						aws.config.update({region: this._configuration.region});
 
-						this._s3 = new aws.S3({apiVersion: this._configuration.apiVersion || '2010-12-01'});
+						this._s3 = new aws.S3({apiVersion: this._configuration.apiVersion || '2006-03-01'});
 					}).then(() => {
 						logger.info('S3 provider started');
 
@@ -52,6 +78,13 @@ module.exports = (() => {
 			return this._startPromise;
 		}
 
+		/**
+		 * Retrieves the contents of a bucket.
+		 *
+		 * @param {string} bucket
+		 *
+		 * @returns {Promise.<object>}
+		 */
 		getBucketContents(bucket) {
 			if (this.getIsDisposed()) {
 				throw new Error('The S3 Provider has been disposed.');
@@ -75,7 +108,18 @@ module.exports = (() => {
 			});
 		}
 
-		uploadObject(bucket, fileName, buffer, mimeType) {
+		/**
+		 * Uploads an object to a bucket.
+		 *
+		 * @param {string} bucket
+		 * @param {string} filename
+		 * @param {string|Buffer} buffer - The content to upload
+		 * @param {string=} mimeType = Defaults to "text/plain"
+		 * @param {boolean=} secure = Indicates if the "private" ACL applies to the object
+		 *
+		 * @returns {Promise.<object>}
+		 */
+		uploadObject(bucket, filename, content, mimeType, secure) {
 			if (this.getIsDisposed()) {
 				throw new Error('The S3 Provider has been disposed.');
 			}
@@ -85,12 +129,32 @@ module.exports = (() => {
 			}
 
 			return new Promise((resolveCallback, rejectCallback) => {
+				let acl;
+
+				if (is.boolean(secure) && secure) {
+					acl = 'private';
+				} else {
+					acl = 'public-read';
+				}
+
+				let mimeTypeToUse;
+
+				if (is.string(mimeType)) {
+					mimeTypeToUse = mimeType;
+				} else if (is.string(content)) {
+					mimeTypeToUse = mimeTypes.text;
+				} else if (is.object) {
+					mimeTypeToUse = mimeTypes.json;
+				} else {
+					throw new Error('Unable to automatically determine MIME type for file.');
+				}
+
 				const params = {
 					Bucket: bucket,
-					Key: fileName,
-					ACL: 'public-read',
-					Body: buffer,
-					ContentType: mimeType
+					Key: filename,
+					ACL: acl,
+					Body: ContentHandler.getHandlerFor(mimeTypeToUse).toBuffer(content),
+					ContentType: mimeTypeToUse
 				};
 
 				const options = {
@@ -109,7 +173,44 @@ module.exports = (() => {
 			});
 		}
 
-		deleteObject(bucket, key) {
+		/**
+		 * Downloads an object from a bucket
+		 *
+		 * @param {string} bucket
+		 * @param {string} filename
+		 *
+		 * @returns {Promise.<object>}
+		 */
+		downloadObject(bucket, filename) {
+			if (this.getIsDisposed()) {
+				throw new Error('The S3 Provider has been disposed.');
+			}
+
+			if (!this._started) {
+				throw new Error('The S3 Provider has not been started.');
+			}
+
+			return new Promise((resolveCallback, rejectCallback) => {
+				this._s3.getObject({ Bucket: bucket, Key: filename }, (err, data) => {
+					if (err) {
+						logger.error('S3 failed to upload object: ', err);
+						rejectCallback(err);
+					} else {
+						resolveCallback(ContentHandler.getHandlerFor(data.ContentType).fromBuffer(data.Body));
+					}
+				});
+			});
+		}
+
+		/**
+		 * Deletes an object from a bucket.
+		 *
+		 * @param {string} bucket
+		 * @param {string} filename
+		 *
+		 * @returns {Promise.<object>}
+		 */
+		deleteObject(bucket, filename) {
 			if (this.getIsDisposed()) {
 				throw new Error('The S3 Provider has been disposed.');
 			}
@@ -121,7 +222,7 @@ module.exports = (() => {
 			return new Promise((resolveCallback, rejectCallback) => {
 				const params = {
 					Bucket: bucket,
-					Key: key
+					Key: filename
 				};
 
 				this._s3.deleteObject(params, (err, data) => {
@@ -141,6 +242,86 @@ module.exports = (() => {
 
 		toString() {
 			return '[S3Provider]';
+		}
+	}
+
+	const contentHandlers = [ ];
+
+	class ContentHandler {
+		constructor() {
+
+		}
+
+		canProcess(mimeType) {
+			return true;
+		}
+
+		toBuffer(content) {
+			return Buffer.from(content);
+		}
+
+		fromBuffer(buffer) {
+			return buffer;
+		}
+
+		static getHandlerFor(mimeType) {
+			if (contentHandlers.length === 0) {
+				contentHandlers.push(new JsonContentHandler());
+				contentHandlers.push(new TextContentHandler());
+				contentHandlers.push(new DefaultContentHandler());
+			}
+
+			return contentHandlers.find(handler => handler.canProcess(mimeType));
+		}
+	}
+
+	class TextContentHandler extends ContentHandler {
+		constructor() {
+			super();
+		}
+
+		canProcess(mimeType) {
+			return mimeType.startsWith('text');
+		}
+
+		toBuffer(content) {
+			if (is.string(content)) {
+				return Buffer.from(content, encodingTypes.utf8);
+			} else {
+				return Buffer.from(content);
+			}
+		}
+
+		fromBuffer(buffer) {
+			return buffer.toString(encodingTypes.utf8);
+		}
+	}
+
+	class JsonContentHandler extends TextContentHandler {
+		constructor() {
+			super();
+		}
+
+		canProcess(mimeType) {
+			return mimeType === mimeTypes.json;
+		}
+
+		toBuffer(content) {
+			if (is.object(content)) {
+				return super.toBuffer(JSON.stringify(content));
+			} else {
+				return super.toBuffer(content);
+			}
+		}
+
+		fromBuffer(buffer) {
+			return JSON.parse(super.fromBuffer(buffer));
+		}
+	}
+
+	class DefaultContentHandler extends ContentHandler {
+		constructor() {
+			super();
 		}
 	}
 
