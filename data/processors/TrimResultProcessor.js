eb7e9d8bd76c2913e267da4083e92b4248715ced
Bryan Ingle <bryan.ingle@barchart.com>
2017-03-22 11:32:22 -0500

    Add unit test and JSDoc for TrimResultProcessor

--- data/processors/TrimResultProcessor.js
+++ data/processors/TrimResultProcessor.js
@@ -10,6 +10,15 @@ module.exports = (() => {
 
 	const logger = log4js.getLogger('data/processors/TrimResultProcessor');
 
+	/**
+	 * Trims a string (or an array of strings). If the value (or array item) is
+	 * not a string, the value is ignored.
+	 *
+	 * @public
+	 * @extends MutateResultProcessor
+	 * @param {object} configuration
+	 * @param {string} configuration.propertyName - The property to trim.
+	 */
 	class TrimResultProcessor extends MutateResultProcessor {
 		constructor(configuration) {
 			super(configuration);
---
+++ test/specs/data/processors/TrimResultProcessorSpec.js
@@ -0,0 +1,74 @@
+var TrimResultProcessor = require('./../../../../data/processors/TrimResultProcessor');
+
+describe('When a TrimResultProcessor is used on a string-based property', function() {
+	'use strict';
+
+	var processor;
+
+	beforeEach(function() {
+		processor = new TrimResultProcessor({ propertyName: 'hair' });
+	});
+
+	describe('and the property value has no leading or trailing spaces', function() {
+		var result;
+		var original;
+
+		beforeEach(function(done) {
+			processor.process(original = { hair: 'yes' })
+				.then(function(r) {
+					result = r;
+					done();
+				});
+		});
+
+		it('the original object should be returned', function() {
+			expect(result).toBe(original);
+		});
+
+		it('the "hair" property value should be unchanged', function() {
+			expect(result.hair).toEqual('yes');
+		});
+	});
+
+	describe('and the property value has leading and trailing spaces', function() {
+		var result;
+		var original;
+
+		beforeEach(function(done) {
+			processor.process(original = { hair: ' club for men ' })
+				.then(function(r) {
+					result = r;
+					done();
+				});
+		});
+
+		it('the original object should be returned', function() {
+			expect(result).toBe(original);
+		});
+
+		it('the "hair" property value should be trimmed', function() {
+			expect(result.hair).toEqual('club for men');
+		});
+	});
+
+	describe('and the property value is a single space', function() {
+		var result;
+		var original;
+
+		beforeEach(function(done) {
+			processor.process(original = { hair: ' ' })
+				.then(function(r) {
+					result = r;
+					done();
+				});
+		});
+
+		it('the original object should be returned', function() {
+			expect(result).toBe(original);
+		});
+
+		it('the "hair" property value should be a zero-length string', function() {
+			expect(result.hair).toEqual('');
+		});
+	});
+});
