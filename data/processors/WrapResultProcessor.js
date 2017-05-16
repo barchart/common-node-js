6cb705273f78afd3d8501b63eaaec003b956bad5
Bryan Ingle <bryan.ingle@barchart.com>
2017-05-16 10:30:15 -0500

    Added documentation

--- data/processors/WrapResultProcessor.js
+++ data/processors/WrapResultProcessor.js
@@ -10,6 +10,17 @@ module.exports = (() => {
 
 	const logger = log4js.getLogger('data/processors/WrapResultProcessor');
 
+	/**
+	 * Wraps the context within an object. Optionally, if the context is an
+	 * array, wraps each item in an object and returns the array of wrapped
+	 * objects.
+	 *
+	 * @public
+	 * @extends ResultProcessor
+	 * @param {object} configuration
+	 * @param {string} configuration.propertyName - The name of the single property inside the wrapped object.
+	 * @param {boolean=} configuration.wrapArray - If true, and the context is an array, returns an array of wrapped object (instead of a single object).
+	 */
 	class WrapResultProcessor extends ResultProcessor {
 		constructor(configuration) {
 			super(configuration);
