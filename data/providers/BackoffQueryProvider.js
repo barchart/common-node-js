b72ced26829d6a8f2ce2850bcf47c3e95363695f
Bryan Ingle <bryaningle@gmail.com>
2017-06-02 08:33:38 -0500

    Backoff query provider now allows custom failure condition

--- data/providers/BackoffQueryProvider.js
+++ data/providers/BackoffQueryProvider.js
@@ -24,6 +24,7 @@ module.exports = (() => {
 	 * @param {Object} configuration
 	 * @param {number=} configuration.millisecondDelay - The amount of time to wait after the first failure. Subsequent failures are multiply this value by 2 ^ [number of failures]. So, a 1000 millisecond backoff would schedule attempts using the following delays: 0, 1000, 2000, 4000, 8000, etc.
 	 * @param {number=} configuration.maximumAttempts - The number of attempts to before giving up.
+	 * @param {Object=} configuration.failureValue - A value that will be considered a failure, and trigger a retry. Equality is determined using a deep comparison. If not supplied, a failing result is anything that's falsey.
 	 */
 	class BackoffQueryProvider extends QueryProvider {
 		constructor(configuration, nestedQueryProvider) {
@@ -36,9 +37,9 @@ module.exports = (() => {
 		_runQuery(criteria) {
 			const configuration = this._getConfiguration();
 
-			return this._scheduler.backoff(() => this._nestedQueryProvider.runQuery(criteria), configuration.millisecondDelay, 'Backoff Query Provider Execution', configuration.maximumAttempts, handleAttemptFailure);
+			return this._scheduler.backoff(() => this._nestedQueryProvider.runQuery(criteria), configuration.millisecondDelay, 'Backoff Query Provider Execution', configuration.maximumAttempts, handleAttemptFailure, configuration.failureValue);
 		}
-		
+
 		toString() {
 			return '[BackoffQueryProvider]';
 		}
