39697b9ece1b077a5274f7c02e3a7882bd3f5565
Bryan Ingle <bryaningle@gmail.com>
2017-07-20 23:31:23 -0500

    Add writable stream for adding records to DynamoDB

---
+++ aws/dynamo/stream/DynamoStreamWriter.js
@@ -0,0 +1,38 @@
+const log4js = require('log4js'),
+	Stream = require('stream');
+
+const assert = require('common/lang/assert'),
+	is = require('common/lang/is');
+
+const DynamoProvider = require('./../../DynamoProvider'),
+	Table = require('./../schema/definitions/Table');
+
+module.exports = (() => {
+	'use strict';
+
+	const logger = log4js.getLogger('common-node/aws/dynamo/stream/DynamoStreamWriter');
+
+	class DynamoStreamWriter extends Stream.Writable {
+		constructor(provider, table) {
+			super({ objectMode: true });
+
+			assert.argumentIsRequired(provider, 'provider', DynamoProvider, 'DynamoProvider');
+			assert.argumentIsRequired(table, 'table', Table, 'Table');
+
+			this._provider = provider;
+			this._table = table;
+		}
+
+		_write(chunk, encoding, callback) {
+			logger.info(chunk.toString());
+
+			setTimeout(callback, 1000);
+		}
+
+		toString() {
+			return '[DynamoStreamWriter]';
+		}
+	}
+
+	return DynamoStreamWriter;
+})();
