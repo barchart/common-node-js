91a11079fb2f12f97a3c90f4dd275fcbb702938f
Bryan Ingle <bryan.ingle@barchart.com>
2017-08-07 16:27:33 -0500

    Delegate based serializer for DynamoDB

---
+++ aws/dynamo/schema/serialization/attributes/DelegateSerializer.js
@@ -0,0 +1,37 @@
+const assert = require('common/lang/assert'),
+	Decimal = require('common/lang/Decimal');
+
+const AttributeSerializer = require('./AttributeSerialzer'),
+	DataType = require('./../../definitions/DataType');
+
+module.exports = (() => {
+	'use strict';
+
+	class DelegateSerializer extends AttributeSerializer {
+		constructor(baseSerializer, serializeDelegate, deserializeDelegate) {
+			super();
+
+			assert.argumentIsRequired(baseSerializer, 'baseSerializer', AttributeSerializer, 'AttributeSerializer');
+			assert.argumentIsRequired(serializeDelegate, 'serializeDelegate', Function);
+			assert.argumentIsRequired(deserializeDelegate, 'deserializeDelegate', Function);
+
+			this._baseSerializer = baseSerializer;
+			this._serializeDelegate = serializeDelegate;
+			this._deserializeDelegate = deserializeDelegate;
+		}
+
+		serialize(value) {
+			return this._baseSerializer.serialize(this._serializeDelegate(value));
+		}
+
+		deserialize(wrapper) {
+			return this._deserializeDelegate(this._baseSerializer.deserialize(wrapper));
+		}
+
+		toString() {
+			return '[DelegateSerializer]';
+		}
+	}
+
+	return DelegateSerializer;
+})();
