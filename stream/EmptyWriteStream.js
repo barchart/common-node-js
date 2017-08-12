a3229059ca26e688e6f9df9aac9465ed4b37a541
Bryan Ingle <bryaningle@gmail.com>
2017-08-12 11:20:38 -0500

    Counter of EmptyWriteStream, and JSDoc

--- stream/EmptyWriteStream.js
+++ stream/EmptyWriteStream.js
@@ -5,12 +5,32 @@ const Stream = require('stream');
 module.exports = (() => {
 	'use strict';
 
+	/**
+	 * A node.js {@link Stream.Writable} that does nothing. By Default,
+	 * the "objectMode" option is set to true.
+	 *
+	 * @public
+	 * @param {Object=} options
+	 */
 	class EmptyWriteStream extends Stream.Writable {
 		constructor(options) {
 			super(object.merge({ objectMode: true  }, (options || { })));
+
+			this._counter = 0;
+		}
+
+		/**
+		 * The number of empty "writes" which have been made.
+		 *
+		 * @returns {Number}
+		 */
+		get count() {
+			return this._counter;
 		}
 
 		_write(chunk, encoding, callback) {
+			this._counter = this._counter + 1;
+
 			callback(null);
 		}
 
