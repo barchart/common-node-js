d9026f3cad8beed867df01f2d7214f37fa75e601
Bryan Ingle <bryaningle@gmail.com>
2017-08-12 16:15:17 -0500

    Transformer for splitting arrays into single items

---
+++ stream/SplitTransformer.js
@@ -0,0 +1,76 @@
+const log4js = require('log4js'),
+	Stream = require('stream');
+
+const assert = require('common/lang/assert'),
+	is = require('common/lang/is'),
+	object = require('common/lang/object'),
+	promise = require('common/lang/promise');
+
+const Transformation = require('./transformations/Transformation');
+
+module.exports = (() => {
+	'use strict';
+
+	const logger = log4js.getLogger('common-node/stream/SplitTransformer');
+
+	/**
+	 * Splits arrays into items.
+	 *
+	 * @public
+	 * @param {String=} description
+	 * @param {Boolean=} silent
+	 */
+	class SplitTransformer extends Stream.Transform {
+		constructor(description, silent) {
+			super({ objectMode: true });
+
+			assert.argumentIsOptional(description, 'description', String);
+			assert.argumentIsOptional(silent, 'silent', Boolean);
+
+			this._description = description || 'Split Transformer';
+			this._silent = is.boolean(silent) && silent;
+
+			this._counter = 0;
+		}
+
+		get transformerCount() {
+			return this._tranformations.length;
+		}
+
+		_transform(chunk, encoding, callback) {
+			this._counter = this._counter + 1;
+
+			let error = null;
+			let output = null;
+
+			if (is.array(chunk)) {
+				chunk.forEach(item => this.push(item));
+
+			} else {
+				error = new Error(`Transformation [ ${this._counter} ] for [ ${this._description} ] failed, unexpected input type.`);
+			}
+
+			if (error === null) {
+				callback();
+			} else {
+				if (this._silent) {
+					logger.warn(`Transformation [ ${this._counter} ] for [ ${this._description} ] failed.`);
+
+					if (logger.isTraceEnabled() && chunk) {
+						logger.trace(chunk);
+					}
+
+					error = null;
+				}
+
+				callback(error, null);
+			}
+		}
+
+		toString() {
+			return '[SplitTransformer]';
+		}
+	}
+
+	return SplitTransformer;
+})();
