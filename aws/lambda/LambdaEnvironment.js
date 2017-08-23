029f74fe39a72ee12fc824692563944792e6f5a6
Bryan Ingle <bryaningle@gmail.com>
2017-08-23 06:53:56 -0500

    JSDoc

--- aws/lambda/LambdaEnvironment.js
+++ aws/lambda/LambdaEnvironment.js
@@ -12,6 +12,8 @@ module.exports = (() => {
 	 * An object that describes the environment of an AWS lambda function.
 	 *
 	 * @public
+	 * @param {String} mode - The function's mode (i.e. dev, stage, or prod).
+	 * @param {String} name - The name of the lambda function.
 	 */
 	return class LambdaEnvironment {
 		constructor(mode, name) {
@@ -21,18 +23,39 @@ module.exports = (() => {
 			this._configurationPromise = null;
 		}
 
+		/**
+		 * Returns the function's mode (i.e. dev, stage, or prod).
+		 *
+		 * @returns {String}
+		 */
 		getMode() {
 			return this._mode;
 		}
 
+		/**
+		 * Returns the name of the function.
+		 *
+		 * @returns {String}
+		 */
 		getName() {
 			return this._name;
 		}
 
+		/**
+		 * Returns true, if the function's mode is production (i.e. "prod");
+		 * otherwise returns false.
+		 *
+		 * @returns {String}
+		 */
 		getIsProduction() {
 			return this._mode === 'prod';
 		}
 
+		/**
+		 * Reads the function's config.yml file and returns the contents.
+		 *
+		 * @returns {Promise.<Object>}
+		 */
 		getConfiguration() {
 			if (this._configurationPromise === null) {
 				this._configurationPromise = Promise.resolve()
@@ -43,6 +66,11 @@ module.exports = (() => {
 			return this._configurationPromise;
 		}
 
+		/**
+		 * Get the singleton instance for the function.
+		 *
+		 * @returns {LambdaEnvironment}
+		 */
 		static getInstance() {
 			if (instance === null) {
 				const matches = process.env.AWS_LAMBDA_FUNCTION_NAME.match(/^(.*)-(dev|stage|prod)$/);
