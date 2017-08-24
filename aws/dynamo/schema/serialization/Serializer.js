219bf0a96abf6d620939060db9d5edf705ccfaee
Bryan Ingle <bryaningle@gmail.com>
2017-08-24 07:46:51 -0500

    Tweak

--- aws/dynamo/schema/serialization/Serializer.js
+++ aws/dynamo/schema/serialization/Serializer.js
@@ -1,5 +1,4 @@
-const assert = require('@barchart/common-js/lang/assert'),
-	is = require('@barchart/common-js/lang/is');
+const assert = require('@barchart/common-js/lang/assert');
 
 const ComponentType = require('./../../schema/definitions/ComponentType'),
 	DataType = require('./../../schema/definitions/DataType'),
