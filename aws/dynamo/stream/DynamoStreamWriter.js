bb795f94ad3d3742336eb790df81a3ccb3277381
Bryan Ingle <bryan.ingle@barchart.com>
2018-06-19 15:16:17 -0500

    DynamoStreamWriter can now be configured to delete records

--- aws/dynamo/stream/DynamoStreamWriter.js
+++ aws/dynamo/stream/DynamoStreamWriter.js
@@ -13,17 +13,16 @@ module.exports = (() => {
 	const logger = log4js.getLogger('common-node/aws/dynamo/stream/DynamoStreamWriter');
 
 	/**
-	 * A Node.js stream writable which accepts single records (or
-	 * arrays of records) and writes them to a DynamoDB table using the
-	 * {@link DynamoProvider#saveItem} (or {@link DynamoProvider#createItems}).
+	 * A Node.js {@link Stream.Writable} which creates or deletes DynamoDB records.
 	 *
 	 * @public
 	 * @extends {Stream.Writable}
-	 * @param {Table} table - The table schema which the items conform to.
+	 * @param {Table} table - The table schema which items must conform to.
 	 * @param {DynamoProvider} provider - The provider used to write records.
+	 * @param {Boolean} remove - If true, the items are deleted (instead of written) to the database.
 	 */
 	class DynamoStreamWriter extends Stream.Writable {
-		constructor(table, provider) {
+		constructor(table, provider, remove) {
 			super({ objectMode: true });
 
 			assert.argumentIsRequired(provider, 'provider', DynamoProvider, 'DynamoProvider');
@@ -31,29 +30,50 @@ module.exports = (() => {
 
 			this._table = table;
 			this._provider = provider;
+
+			let delegateFactory;
+
+			if (is.boolean(remove) && remove) {
+				delegateFactory = getCreateDelegate;
+			} else {
+				delegateFactory = getDeleteDelegate;
+			}
+
+			this._delegateFactory = delegateFactory;
 		}
 
 		_write(chunk, encoding, callback) {
-			let writePromise;
+			let delegate = this._delegateFactory(chunk);
 
-			if (is.array(chunk)) {
-				writePromise = this._provider.createItems(chunk, this._table);
-			} else {
-				writePromise = this._provider.saveItem(chunk, this._table);
-			}
+			delegate.call(this._provider, chunk, this._table)
+				.then(() => {
+					callback(null);
+				}).catch((e) => {
+					logger.error('Failed to write chunk', e);
+					logger.error('Failed to write chunk', JSON.stringify((chunk || {}), null, 2));
 
-			writePromise.then(() => {
-				callback(null);
-			}).catch((e) => {
-				logger.error('Failed to write chunk', e);
-				logger.error('Failed to write chunk', JSON.stringify((chunk || {}), null, 2));
-
-				callback(e);
-			});
+					callback(e);
+				});
 		}
 
 		toString() {
 			return '[DynamoStreamWriter]';
+		}
+	}
+
+	function getCreateDelegate(chunk) {
+		if (is.array(chunk)) {
+			return this._provider.createItems;
+		} else {
+			return this._provider.saveItem;
+		}
+	}
+
+	function getDeleteDelegate(chunk) {
+		if (is.array(chunk)) {
+			return this._provider.deleteItems;
+		} else {
+			return this._provider.deleteItem;
 		}
 	}
 
