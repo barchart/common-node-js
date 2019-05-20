068befd676d0c2a395496caa14f094d0fd0fc7a3
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2019-05-20 11:27:03 -0500

    Remove unused import

--- data/providers/BackoffQueryProvider.js
+++ data/providers/BackoffQueryProvider.js
@@ -1,7 +1,6 @@
 const log4js = require('log4js');
 
-const object = require('@barchart/common-js/lang/object'),
-	Scheduler = require('@barchart/common-js/timing/Scheduler');
+const Scheduler = require('@barchart/common-js/timing/Scheduler');
 
 const QueryProvider = require('./../QueryProvider');
 
