ac515bb237ad85599c7da50bf32c7a5d90b5660d
Igor Losev <igor.losev.it@gmail.com>
2019-09-03 14:06:38 +0300

    Add Map data type for dynamoDB

---
+++ test/specs/aws/dynamo/schema/serialization/attributes/MapSerializerSpec.js
@@ -0,0 +1,23 @@
+var MapSerializer = require('./../../../../../../../aws/dynamo/schema/serialization/attributes/MapSerializer');
+
+describe('When a MapSerializer is instantiated', function() {
+	'use strict';
+
+	var serializer;
+
+	beforeEach(function() {
+		serializer = new MapSerializer();
+	});
+
+	it('it serializes { "Name": "Joe", "Age": 35 } as { "M": {"Name": {"S": "Joe"}, "Age": {"N": "35"}} }', function() {
+		var serialized = serializer.serialize({ Name: 'Joe', Age: 35 });
+
+		expect(serialized.M).toEqual({ Name: { S: 'Joe' }, Age: { N: '35' } });
+	});
+
+	it('it deserializes { "M": {"Name": {"S": "Joe"}, "Age": {"N": "35"}} } as { "Name": "Joe", "Age": 35 }', function() {
+		var deserialized = serializer.deserialize({ M: { Name: { S: 'Joe' }, Age: { N: 35 } } });
+
+		expect(deserialized).toEqual({ Name: 'Joe', Age: 35 });
+	});
+});
