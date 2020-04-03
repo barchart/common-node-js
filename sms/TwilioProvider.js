b8753199cb6e6f04a782627e3e4671c1b7801f8f
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-04-02 19:15:28 -0500

    Refactor, return SID, fix array of recipients (still not perfect)

--- sms/TwilioProvider.js
+++ sms/TwilioProvider.js
@@ -4,7 +4,6 @@ const log4js = require('log4js'),
 const assert = require('@barchart/common-js/lang/assert'),
 	is = require('@barchart/common-js/lang/is'),
 	Disposable = require('@barchart/common-js/lang/Disposable'),
-	promise = require('@barchart/common-js/lang/promise'),
 	RateLimiter = require('@barchart/common-js/timing/RateLimiter');
 
 module.exports = (() => {
@@ -29,96 +28,142 @@ module.exports = (() => {
 
 			assert.argumentIsOptional(configuration.rateLimitPerSecond, 'configuration.rateLimitPerSecond', Number);
 
-			this._publisher = null;
-
 			this._configuration = configuration;
 
 			this._startPromise = null;
 			this._started = false;
 
-			this._rateLimiter = new RateLimiter(configuration.rateLimitPerSecond || 10, 1000);
+			this._client = null;
+			this._rateLimiter = null;
+
+			this._counter = 0;
 		}
 
 		start() {
-			if (this.getIsDisposed()) {
-				throw new Error('The Twilio provider has been disposed.');
-			}
-
 			if (this._startPromise === null) {
 				this._startPromise = Promise.resolve()
 					.then(() => {
-						this._publisher = twilio(this._configuration.accountSid, this._configuration.authToken);
-					}).then(() => {
 						logger.info('Twilio provider started');
+
+						this._client = twilio(this._configuration.accountSid, this._configuration.authToken);
+						this._rateLimiter = new RateLimiter(this._configuration.rateLimitPerSecond || 10, 1000);
 
 						this._started = true;
 
-						return this._started;
+						return Promise.resolve(true);
 					}).catch((e) => {
 						logger.error('Twilio provider failed to start', e);
 
-						throw e;
+						return Promise.reject(false);
 					});
 			}
 
 			return this._startPromise;
 		}
 
-		sendSms(recipientNumber, content, sourceNumber) {
-			if (is.array(recipientNumber)) {
-				assert.argumentIsArray(recipientNumber, 'recipientNumber', String);
-			} else {
-				assert.argumentIsRequired(recipientNumber, 'recipientNumber', String);
-			}
+		/**
+		 * Sends an SMS message to one (or more) phone numbers.
+		 *
+		 * @public
+		 * @param {String|Array<String>} recipients
+		 * @param {String} content
+		 * @param {String=} source
+		 * @returns {Promise<String|Array<String>>}
+		 */
+		sendSms(recipients, content, source) {
+			return Promise.resolve()
+				.then(() => {
+					if (is.array(recipients)) {
+						assert.argumentIsArray(recipients, 'recipients', String);
+					} else {
+						assert.argumentIsRequired(recipients, 'recipients', String);
+					}
 
-			assert.argumentIsRequired(content, 'content', String);
-			assert.argumentIsOptional(sourceNumber, 'sourceNumber', String);
+					assert.argumentIsRequired(content, 'content', String);
+					assert.argumentIsOptional(sourceNumber, 'source', String);
 
-			if (this.getIsDisposed()) {
-				throw new Error('The Twilio provider has been disposed.');
-			}
+					checkReady.call(this);
 
-			if (!this._started) {
-				throw new Error('The Twilio provider has not been started.');
-			}
+					let recipientsToUse;
+					let recipientsPlural;
+					
+					if (this._configuration.recipientOverride) {
+						recipientsToUse = [ this._configuration.recipientOverride ];
+						recipientsPlural = false;
+					} else if (is.array(recipients)) {
+						recipientsToUse = recipients;
+						recipientsPlural = true;
+					} else {
+						recipientsToUse = [ recipients ];
+						recipientsPlural = false;
+					}
+					
+					let sourceToUse;
 
-			if (this._configuration.recipientOverride) {
-				logger.warn('Overriding sms recipient for testing purposes.');
+					if (source) {
+						sourceToUse = source;
+					} else {
+						sourceToUse = this._configuration.sourceNumber;
+					}
 
-				recipientNumber = this._configuration.recipientOverride;
-			}
+					const sendAction = () => {
+						return Promise.all(recipientsToUse.map((recipient) => {
+							const id = this._counter++;
 
-			let recipientNumbersToUse;
+							const payload = { };
 
-			if (is.array(recipientNumber)) {
-				recipientNumbersToUse = recipientNumber;
-			} else {
-				recipientNumbersToUse = [recipientNumber];
-			}
+							payload.from = sourceToUse;
+							payload.to = recipient;
+							payload.body = content;
 
-			const sourceNumberToUse = sourceNumber || this._configuration.sourceNumber;
+							logger.debug('Sending SMS [', id, '] via Twilio to [', recipient, ']');
 
-			return this._rateLimiter.enqueue(() => {
-				return promise.build((resolveCallback, rejectCallback) => {
-					recipientNumbersToUse.forEach((targetNumber) => {
-						logger.debug('Sending sms via twilio to', targetNumber);
+							return this._client.messages.create(payload)
+								.then((response) => {
+									logger.debug('Sent SMS [', id, '] via Twilio to [', recipient, '] with SID [', response.sid, ']');
 
-						return this._publisher.messages.create({ from: sourceNumberToUse, to: targetNumber, body: content }, () => {
-							resolveCallback();
+									return response.sid;
+								}).catch((e) => {
+									logger.error('Failed to send SMS [', id, '] via Twilio to [', recipient, ']', e);
+
+									return null;
+								});
+						})).then((responses) => {
+							if (recipientsPlural) {
+								return responses;
+							} else {
+								return responses[0];
+							}
 						});
-					});
+					};
+
+					return this._rateLimiter.enqueue(sendAction);
 				});
-			});
 		}
 
 		_onDispose() {
-			this._rateLimiter.dispose();
+			logger.debug('Twilio disposed');
 
-			logger.debug('Twilio provider disposed');
+			if (this._rateLimiter !== null) {
+				this._rateLimiter.dispose();
+			}
+
+			this._rateLimiter = null;
+			this._client = null;
 		}
 
 		toString() {
 			return '[TwilioProvider]';
+		}
+	}
+	
+	function checkReady() {
+		if (this.getIsDisposed()) {
+			throw new Error('The Twilio provider has been disposed.');
+		}
+
+		if (!this._started) {
+			throw new Error('The Twilio Provider has not been started.');
 		}
 	}
 
