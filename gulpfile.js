4fbecad43865ed1a20412cc8d29208653291883c
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-04-11 18:27:05 -0500

    Add build file for AWS CodeBuild CI

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,8 @@ gulp.task('release', gulp.series(
 gulp.task('lint', () => {
     return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./test/dist/**', '!./docs/**' ])
         .pipe(jshint({'esversion': 6}))
-        .pipe(jshint.reporter('default'));
+        .pipe(jshint.reporter('default'))
+		.pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test', gulp.series('execute-tests'));
