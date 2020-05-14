f6a67b97648013348acc13da55859b1ef7865dd3
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-05-14 14:17:55 -0500

    Upgrade JSHint (now uses ES2019)

--- gulpfile.js
+++ gulpfile.js
@@ -84,9 +84,9 @@ gulp.task('release', gulp.series(
 ));
 
 gulp.task('lint', () => {
-    return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./test/dist/**' ])
-        .pipe(jshint({'esversion': 6}))
-        .pipe(jshint.reporter('default'))
+	return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./test/dist/**' ])
+		.pipe(jshint({ esversion: 9 }))
+		.pipe(jshint.reporter('default'))
 		.pipe(jshint.reporter('fail'));
 });
 
