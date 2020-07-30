36daac57359c62b39bcd42cb465ea1560866149a
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-07-30 09:56:33 -0500

    Correct JSDoc

--- aws/lambda/LambdaSecretsManager.js
+++ aws/lambda/LambdaSecretsManager.js
@@ -57,7 +57,7 @@ module.exports = (() => {
 	/**
 	 * @function
 	 * @private
-	 * @returns {SecretsManagerProvider}
+	 * @returns {Promise<SecretsManagerProvider>}
 	 */
 	function getSecretsManagerProvider() {
 		if (secretsManagerProviderPromise === null) {
