fc214fdc36291b325b5ab5af00f8ed178f221d4e
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-12-01 16:14:56 -0700

    Fix

--- aws/lambda/validators/LambdaEventValidator.js
+++ aws/lambda/validators/LambdaEventValidator.js
@@ -91,7 +91,7 @@ module.exports = (() => {
 							return messageValidator.validate(name, message, event, trigger, messageId)
 								.then((valid) => {
 									if (!valid) {
-										logger.warn(`Message [ ${i.toString()} ] rejected by [ ${messageValidator.toString()} ]`)
+										logger.warn(`Message [ ${i.toString()} ] rejected by [ ${messageValidator.toString()} ]`);
 									}
 
 									return valid;
