ada7afdcbfff07deef8e1351d6b9e8d4bd045f68
Luka Sotra <lukasotra@gmail.com>
2025-03-06 03:10:40 +0100

    feat: create `sendEmail` script

---
+++ test/specs/scripts/sendEmail.js
@@ -0,0 +1,27 @@
+const SesProvider = require('../../../aws/SesProvider');
+
+async function sendEmail() {
+    const configuration = {
+        region: 'us-east-1'
+    };
+
+    const sesProvider = new SesProvider(configuration);
+    await sesProvider.start();
+
+    const senderAddress = 'luka.sotra@barchart.com';
+    const recipientAddress = 'luka.sotra@barchart.com';
+    const subject = 'Test Email';
+    const htmlBody = '<h1>Test</h1><p>email.</p>';
+    const textBody = 'This is a test email.';
+
+    try {
+        await sesProvider.sendEmail(senderAddress, recipientAddress, subject, htmlBody, textBody);
+        console.log('Email sent successfully');
+    } catch (error) {
+        console.error('Failed to send email', error);
+    }
+}
+
+sendEmail().catch(error => {
+    console.error('Error:', error);
+});
